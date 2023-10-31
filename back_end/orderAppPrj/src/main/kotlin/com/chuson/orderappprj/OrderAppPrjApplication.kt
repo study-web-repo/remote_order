26ff240eb449d2712c25c1844dca877cfbe4e982
추성훈 <licsh123@naver.com>
2023-11-01 00:45:42 +0900

    #2-springboot start
    spring-boot start

---
+++ back_end/orderAppPrj/src/main/kotlin/com/chuson/orderappprj/OrderAppPrjApplication.kt
@@ -0,0 +1,11 @@
+package com.chuson.orderappprj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderAppPrjApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderAppPrjApplication>(*args)
+}
