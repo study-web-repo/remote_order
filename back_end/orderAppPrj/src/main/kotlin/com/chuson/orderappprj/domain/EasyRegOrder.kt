3056901f4f13f3215a7f91514ec5ea70a210fcef
추성훈 <licsh123@naver.com>
2023-11-01 01:04:52 +0900

    #3-domain 클래스 설계
    간편주문 도메인 설계

---
+++ back_end/orderAppPrj/src/main/kotlin/com/chuson/orderappprj/domain/EasyRegOrder.kt
@@ -0,0 +1,11 @@
+package com.chuson.orderappprj.domain
+
+class EasyRegOrder{
+    private var companyName : String =""
+    private var companyId : Long = 0
+    private var companyInfo : String = ""
+
+    private var createdBy : String = ""
+    private var modifiedBy : String = ""
+
+}
